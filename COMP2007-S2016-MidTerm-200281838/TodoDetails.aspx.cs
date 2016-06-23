31c72fcbc9abf65531ffee3d33da44652ea6c756
Chaosman004 <mr.robert.page@gmail.com>
2016-06-23 18:23:25 -0400

    TodoDetails built - missing check box functonality, but can add and edit todos

--- COMP2007-S2016-MidTerm-200281838/TodoDetails.aspx.cs
+++ COMP2007-S2016-MidTerm-200281838/TodoDetails.aspx.cs
@@ -5,13 +5,92 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// using statements required for EF DB access
+using COMP2007_S2016_MidTerm_200281838.Models;
+using System.Web.ModelBinding;
+
 namespace COMP2007_S2016_MidTerm_200281838
 {
     public partial class TodoDetails : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if ((!IsPostBack) && (Request.QueryString.Count > 0))
+            {
+                this.GetTodo();
+            }
+        }
 
+        protected void GetTodo()
+        {
+            // populate teh form with existing data from the database
+            int TodoID = Convert.ToInt32(Request.QueryString["TodoID"]);
+
+            // connect to the EF DB
+            using (TodoConnection db = new TodoConnection())
+            {
+                // populate a Todo object instance with the TodoID from the URL Parameter
+                Todo updatedTodo = (from Todo in db.Todos
+                                       where Todo.TodoID == TodoID
+                                       select Todo).FirstOrDefault();
+
+                // map the Todo properties to the form controls
+                if (updatedTodo != null)
+                {
+                    TodoNameTextBox.Text = updatedTodo.TodoName;
+                    CompletedTextBox.Text = Convert.ToString(updatedTodo.Completed);
+                    TodoNotesTextBox.Text = updatedTodo.TodoNotes;
+                }
+            }
+        }
+
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            // Redirect back to TodoList page
+            Response.Redirect("~/TodoList.aspx");
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            // Use EF to connect to the server
+            using (TodoConnection db = new TodoConnection())
+            {
+                // use the Todo model to create a new Todo object and
+                // save a new record
+                Todo newTodo = new Todo();
+
+                int TodoID = 0;
+
+                if (Request.QueryString.Count > 0) // our URL has a StudentID in it
+                {
+                    // get the id from the URL
+                    TodoID = Convert.ToInt32(Request.QueryString["TodoID"]);
+
+                    // get the current student from EF DB
+                    newTodo = (from Todo in db.Todos
+                               where Todo.TodoID == TodoID
+                               select Todo).FirstOrDefault();
+                }
+
+                // add form data to the new student record
+                newTodo.TodoName = TodoNameTextBox.Text;
+                newTodo.Completed = Convert.ToBoolean(CompletedTextBox.Text);
+                newTodo.TodoNotes = TodoNotesTextBox.Text;
+
+                // use LINQ to ADO.NET to add / insert new student into the database
+
+                if (TodoID == 0)
+                {
+                    db.Todos.Add(newTodo);
+                }
+
+
+                // save our changes - also updates and inserts
+                db.SaveChanges();
+
+                // Redirect back to the updated students page
+                Response.Redirect("~/TodoList.aspx");
+            }
         }
     }
 }
